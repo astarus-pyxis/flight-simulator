2f3515f50b62cd97433229ae448853663ba9cf7b
Arthur JOLIVET <arthur.jolivet@student.isae-supaero.fr>
2024-11-27 23:18:16 +0100

    turn (untested) and fixes

--- src/physics_engine.py
+++ src/physics_engine.py
@@ -3,23 +3,24 @@ import math
 ## Environment constants
 
 G = 9.81
-RHO = 1.225
 
 ## Plane constants
 
 MASS = 100_000
 NB_ENGINES = 2
-ENGINE_THRUST = 180_000 #in N
+ENGINE_THRUST = 180_000 # in N
 WING_SURFACE = 180
 STALL_ANGLE_DEG = 15
 STALL_ANGLE = STALL_ANGLE_DEG * math.pi/180
 CL_MAX = 1.3
+STATIC_MARGIN = 0.05*5 # in meters
 
 ## Plane main variables
 
-pitch = 3 * math.pi/180 #radian
+pitch = 12 * math.pi/180 #radian
 roll = 0 #radian
-altitude = 1000 #metres
+altitude = 0 #metres
+heading = 0 #radian
 vz = 0 #(vertical speed, m/s)
 vx = 100 #(horizontal speed, m/s)
 throttle = 1 #Between 0 and 1
@@ -34,13 +35,22 @@ lift = 0
 drag = 0
 slope = 0
 
+rho = 0
+
+def compute_rho(altitude):
+    return 352.995 * (1-0.0000225577*altitude)**5.25516 / (288.15 - 0.0065*altitude)
+
+def update_rho():
+    global rho
+    rho = compute_rho(altitude)
+
 def update_speed():
     global speed
     speed = math.sqrt(vz**2 + vx**2)
 
 def update_aoa():
     global aoa
-    aoa = pitch - math.asin(vz/speed)
+    aoa = (pitch - math.asin(vz/speed))*math.cos(roll)
 
 def compute_cl(aoa):
     
@@ -72,7 +82,7 @@ def update_cd():
     global cd
 
     def polar(cl):
-        return 0.02 + 0.05*cl**2
+        return 0.02 + 0.06*cl**2
 
     if abs(aoa) < STALL_ANGLE:
         cd = polar(cl)
@@ -82,15 +92,15 @@ def update_cd():
 
 def update_thrust():
     global thrust
-    thrust = NB_ENGINES * throttle * ENGINE_THRUST
+    thrust = NB_ENGINES * throttle * ENGINE_THRUST * rho / compute_rho(0)
 
 def update_lift():
     global lift
-    lift = 1/2 * RHO * WING_SURFACE * speed**2 * cl
+    lift = 1/2 * rho * WING_SURFACE * speed**2 * cl
 
 def update_drag():
     global drag
-    drag = 1/2 * RHO * WING_SURFACE * speed**2 * cd
+    drag = 1/2 * rho * WING_SURFACE * speed**2 * cd
 
 def update_slope():
     global slope
@@ -112,7 +122,12 @@ def update_vx(dt):
     horizontal_force = lift * -math.sin(slope) - drag * math.cos(slope) + thrust * math.cos(pitch)
     vx += horizontal_force * dt / MASS
 
+def update_heading(dt):
+    global heading
+    heading += dt * STATIC_MARGIN * math.sin(roll) * lift
+
 def update(dt): #dt is supposed to be small
+    update_rho()
     update_speed()
     update_aoa()
     update_cl()
@@ -124,6 +139,7 @@ def update(dt): #dt is supposed to be small
     update_vz(dt)
     update_altitude(dt)
     update_vx(dt)
+    update_heading(dt)
 
 
 ## Tests
@@ -152,12 +168,13 @@ import matplotlib.pyplot as plt
 
 # Simulation
 
-duration = 600
-dt = 0.01
+duration = 6000
+dt = 0.1
 
 time = [i*dt for i in range(int(duration/dt))]
 altitude_r = []
 speed_r = []
+thrust_r = []
 vz_r = []
 vx_r = []
 aoa_r = []
@@ -170,6 +187,7 @@ for t in time:
     update(dt)
     altitude_r.append(altitude)
     speed_r.append(speed)
+    thrust_r.append(thrust)
     vz_r.append(vz)
     vx_r.append(vx)
     aoa_r.append(aoa)
@@ -179,10 +197,11 @@ for t in time:
     lift_r.append(lift)
     drag_r.append(drag)
 
-print_index = 1755
+print_index = 0
 
 print("altitude", altitude_r[print_index])
 print("speed", speed_r[print_index])
+print("thrust", thrust_r[print_index])
 print("vz", vz_r[print_index])
 print("vx", vx_r[0])
 print("aoa", aoa_r[print_index])
@@ -192,13 +211,15 @@ print("cd", cd_r[print_index])
 print("lift", lift_r[print_index])
 print("drag", drag_r[print_index])
 
-start_,stop_ = 0,600 #175,175.7
+start_,stop_ = 0,duration #175,175.7
 start,stop = int(start_/dt),int(stop_/dt)
 
 plt.figure("altitude")
 plt.plot(time[start:stop],altitude_r[start:stop])
 plt.figure("speed")
 plt.plot(time[start:stop],speed_r[start:stop])
+plt.figure("thrust")
+plt.plot(time[start:stop],thrust_r[start:stop])
 plt.figure("vz")
 plt.plot(time[start:stop],vz_r[start:stop])
 plt.figure("vx")
@@ -210,7 +231,8 @@ plt.plot(time[start:stop],slope_r[start:stop])
 plt.figure("cl, cd")
 plt.plot(time[start:stop], cl_r[start:stop])
 plt.plot(time[start:stop], cd_r[start:stop])
-plt.figure("lift, drag")
+plt.figure("lift")
 plt.plot(time[start:stop], lift_r[start:stop])
+plt.figure("drag")
 plt.plot(time[start:stop], drag_r[start:stop])
 plt.show()
